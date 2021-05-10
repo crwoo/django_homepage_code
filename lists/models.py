8777907339e645873367457941eab77dd4db456d
crwoo <dananoplat@gmail.com>
2021-05-11 01:30:29 +0900

    2021-05-11 2, add conversation model

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core import models as core_md
 
 # Create your models here.
+
+
+class List(core_md.TimeStampModel):
+
+    """List Model Definition"""
+
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    restaurants = models.ManyToManyField("restaurants.Restaurant", blank=True)
+
+    def __str__(self):
+        return self.name
