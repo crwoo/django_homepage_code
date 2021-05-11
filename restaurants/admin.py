a62a7b2654daec1f6250a1ba18f2c059c593a52b
crwoo <dananoplat@gmail.com>
2021-05-12 00:24:41 +0900

    2021-05-12 adding restaurant adnim models

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -16,6 +16,26 @@ class ItemAdmin(admin.ModelAdmin):
 class RestaurantAdmin(admin.ModelAdmin):
     """restaurant admin Definitions"""
 
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "address",
+        "instant_book",
+        "restaurant_type",
+        "owner",
+    )
+
+    list_filter = (
+        "instant_book",
+        "city",
+        "country",
+    )
+
+    # 검색으로 도시 찾을 수 있음
+    # owner__ <언더바 두개 = foreign key
+    search_fields = ("^city", "owner__username", "name")
+
     pass
 
 
